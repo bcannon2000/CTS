8d4178e889ae0432059d862b486f08adc7957f7f
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 15:18:35 +0000

    Implemented styles for the initial blog layout

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,8 @@
 class PortfoliosController < ApplicationController
-before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  
   def index
     @portfolio_items = Portfolio.all
   end
@@ -27,11 +29,9 @@ before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   end
 
   def edit
-    3.times { @portfolio_item.technologies.build }
   end
 
   def update
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -45,8 +45,6 @@ before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   end
 
   def destroy
-    # Perform the lookup
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -65,7 +63,7 @@ before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
                                       technologies_attributes: [:name]
                                      )
   end
-  
+
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
