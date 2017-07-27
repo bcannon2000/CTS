ff7dd0c07d8a8eaf7a3380547d37aedcc2856071
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 15:07:22 +0000

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,7 @@ module ApplicationHelper
   def copyright_generator
     CannonTools::Renderer.copyright 'Benjamin Cannon', 'All rights reserved'
   end
-  
+
   def nav_items
     [
       {
@@ -29,35 +29,46 @@ module ApplicationHelper
       {
         url: about_me_path,
         title: 'About Me'
-      },    
+      },
       {
         url: contact_path,
         title: 'Contact'
       },
       {
         url: blogs_path,
-        title: 'Blogs'
+        title: 'Blog'
       },
       {
         url: portfolios_path,
         title: 'Portfolio'
-      },      
+      },
     ]
   end
-  
+
   def nav_helper style, tag_type
     nav_links = ''
-    
+
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
-      
-    
+
     nav_links.html_safe
   end
-  
+
   def active? path
     "active" if current_page? path
   end
-  
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Benjamin Cannon Portfolio", sticky: false)
+  end
+
 end
